8b7022497baaca8f9eeb9ef88923b60da60cee7b
Kunal Ganglani <kunalganglani@gmail.com>
2018-12-08 02:39:18 +0530

    localstorage added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageDataService } from './services/image-data.service';
+import * as localForage from 'localforage';
 
 @Component({
   selector: 'app-root',
@@ -14,18 +15,22 @@ export class AppComponent implements OnInit {
   this.setDataSource();
   }
   setDataSource() {
-    this._imageDataService.getAllImages()
-    .subscribe(
-      res => {
-        this.galleryImages = res;
-        // this.windowHeaderData = res[0];
-        localStorage.setItem('galleryImages', JSON.stringify(res));
-      },
-      err => {
-        console.log('service failed');
-        // this.openSnackBar(`Error: ${err.error.text}`, 'Register a few users');
-      }
-    );
+    if (!localStorage.getItem('galleryImages')) {
+      this._imageDataService.getAllImages()
+      .subscribe(
+        res => {
+          this.galleryImages = res;
+          // this.windowHeaderData = res[0];
+          localStorage.setItem('galleryImages', JSON.stringify(res));
+        },
+        err => {
+          console.log('service failed');
+          // this.openSnackBar(`Error: ${err.error.text}`, 'Register a few users');
+        }
+      );
+    } else {
+      this.galleryImages = JSON.parse(localStorage.getItem('galleryImages'));
+    }
   }
 
   ngOnInit(): void {
