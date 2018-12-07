6001e641593efc2c1022e9cef6e8c5497ff5e6a1
Kunal Ganglani <kunalganglani@gmail.com>
2018-12-07 15:04:35 +0530

    image data service from firebase added with masonry layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImageDataService } from './services/image-data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  galleryImages = [];
+
+  constructor(private _imageDataService: ImageDataService) {
+  this.setDataSource();
+  }
+  setDataSource() {
+    this._imageDataService.getAllImages()
+    .subscribe(
+      res => {
+        this.galleryImages = res;
+        // this.windowHeaderData = res[0];
+        localStorage.setItem('galleryImages', JSON.stringify(res));
+      },
+      err => {
+        console.log('service failed');
+        // this.openSnackBar(`Error: ${err.error.text}`, 'Register a few users');
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.setDataSource();
+  }
 }
+
---
+++ src/app/services/image-data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageDataService {
+  url = 'http://localhost:3000';
+  prodUrl = 'https://kunalgallery.firebaseio.com';
+  constructor(private _http: HttpClient) { }
+
+  getAllImages() {
+    // return this._http.get<any>(this.url + '//imageResults');
+    return this._http.get<any>(this.prodUrl + '/imageResults.json');
+  }
+  getImageById(id) {
+    // return this._http.get<any>(this.url + '//imageResults' + id);
+    return this._http.get<any>(this.prodUrl + `/imageResults/${id}.json`);
+  }
